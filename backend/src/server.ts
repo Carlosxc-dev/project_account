8d5878e6918293eaca45c6eca28d0a18ca06e5e6
Carlosxc-dev <chcruz64@gmail.com>
2024-08-23 17:27:32 -0300

    up para ec2 com docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,7 @@
 import { app } from "./app";
 
-const port = process.env.SERVER_PORT || 3030;
+const port = process.env.SERVER_PORT || 3001;
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
-
