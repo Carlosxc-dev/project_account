c2d49233d95b02aa457010a019b5c2f91cc3e6f8
carlos <chcruz64@gmail.com>
2024-09-06 12:32:36 -0300

    deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: 'https://project-account-frontend.onrender.com/',
+		origin: 'https://project-account-frontend.onrender.com',
 		credentials: true,
 	})
 );
